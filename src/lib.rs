dc55608c5b6965253fb3d720842040a1c08b77ce
schooljack <jack.greenberg@icsd.k12.ny.us>
2018-11-14 20:51:54 +0000

    Small packet generation updates and implemented Default for DriverStation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::default::Default;
+
 #[derive(Clone)]
 enum JoystickType {
     Unknown = -1,
@@ -60,7 +62,7 @@ impl Joystick {
         }
     }
 
-    fn to_tag(&self) -> Vec<u8> {
+    fn udp_tag(&self) -> Vec<u8> {
         let mut tag: Vec<u8> = Vec::new();
 
         tag.push(self.axes.len() as u8);
@@ -134,7 +136,7 @@ enum MatchType {
     Elimination = 3,
 }
 
-struct DriverStation {
+pub struct DriverStation {
     joysticks: Vec<Option<Joystick>>,
     estop: bool,
     enabled: bool,
@@ -142,51 +144,66 @@ struct DriverStation {
     alliance: Alliance,
     game_data: String,
     competition: String,
-	sequence_num: u16
+    sequence_num: u16,
 }
 
 impl DriverStation {
-    fn new() -> Self {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    fn udp_packet(&self) -> Vec<u8> {
+        let mut packet: Vec<u8> = Vec::new();
+
+        packet.push(((self.sequence_num >> 8) & 0xff) as u8);
+        packet.push((self.sequence_num & 0xff) as u8);
+
+        packet.push(0x01); // comm version
+        packet.push(self.control_byte()); // control byte
+        packet.push(0); // TODO: actually restart code or rio with this byte.
+        packet.push(self.alliance.to_position_u8()); // alliance
+
+        // joystick tags
+        for stick in &self.joysticks {
+            if let Some(stick) = stick {
+                let mut tag = stick.udp_tag();
+                packet.push(tag.len() as u8 + 1);
+                packet.push(0x0c);
+                packet.extend(tag);
+            }
+            // TODO: add something for missing sticks?
+        }
+
+        packet
+    }
+
+    fn control_byte(&self) -> u8 {
+        let mut byte: u8 = 0;
+        if self.estop {
+            byte |= 0b1000_0000;
+        }
+        // fms is never connected, but if it were that would go here
+        if self.enabled {
+            byte |= 0b0000_0100;
+        }
+
+        byte |= self.mode as u8;
+
+        byte
+    }
+}
+
+impl Default for DriverStation {
+    fn default() -> Self {
         DriverStation {
             joysticks: vec![None; 6],
             estop: false,
             enabled: false,
             mode: RobotMode::Teleop,
             alliance: Alliance::Red(1),
-            game_data: String::from("rrr"),
+            game_data: String::new(),
             competition: String::from("unknown"),
-			sequence_num: 0
+            sequence_num: 0,
         }
     }
-
-	fn udp_packet(&self) -> Vec<u8> {
-		let mut packet: Vec<u8> = Vec::new();
-
-		packet.push(((self.sequence_num >> 8) & 0xff) as u8 );
-		packet.push((self.sequence_num & 0xff) as u8);
-
-		packet.push(0x01); // comm version
-		packet.push(self.control_byte()); // control byte
-		packet.push(0); // TODO: actually restart code or rio with this byte.
-		packet.push(self.alliance.to_position_u8()); // alliance
-
-		// TODO: joystick tags
-
-		packet
-	}
-
-	fn control_byte(&self) -> u8 {
-		let mut byte: u8 = 0;
-		if self.estop {
-			byte |= 0b1000_0000;
-		}
-		// fms is never connected, but if it were that would go here
-		if self.enabled {
-			byte |= 0b0000_0100;
-		}
-
-		byte |= self.mode as u8;
-
-		byte
-	}
 }
